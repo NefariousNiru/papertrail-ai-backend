91c501bfdab0d28661c35b59e784ba446970e8a0
Nirupom Bose Roy <80672469+NefariousNiru@users.noreply.github.com>
2025-10-08 01:06:17 -0400

    Semantic scholar api (#4)
    * Improve Prompt
    
    * Update README.md
    
    * Add Semantic Scholar

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 # routes.py
 from fastapi import FastAPI
-
 from controller.paper_controller import paper_router
+from controller.semantic_search_controller import semantic_search_router
 from controller.validation_controller import validation_router
 
 
@@ -9,3 +9,4 @@ def register_routes(app: FastAPI) -> None:
     """Register & Access control controllers here."""
     app.include_router(validation_router)
     app.include_router(paper_router)
+    app.include_router(semantic_search_router)
